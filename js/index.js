3e41e9bff622e7c593f4a0ffbcee9c25a9d3529a
Maicon Douglas <maiconboy11@gmail.com>
2024-10-18 11:25:02 -0300

    1

---
+++ js/index.js
@@ -0,0 +1,105 @@
+document.querySelector('.btn-primary').addEventListener('click', function(){
+    var area = document.querySelector('.section')
+    var textoElement = document.getElementById('texto_oculto')
+
+    if (textoElement.style.display === "none") {
+        textoElement.style.display = "block";
+        setTimeout(() => {
+            textoElement.style.opacity = '1'
+        }, 100);
+    } else {
+        textoElement.style.display = "none";
+        setTimeout(() => {
+            textoElement.style.opacity = '0'
+        }, 100);
+    }
+})
+
+
+
+gsap.to('.pilares_container',{
+    y:20,
+    opacity:1,
+    duration:1,
+    scrollTrigger:{
+        trigger:'.paragrafo2-container',
+        start:'bottom center',
+        
+        markers:false
+    }})
+
+
+    gsap.to(".animated-paragrafo1", {
+        y: 0,  // Sobe para a posição original
+        duration: 1,  // Duração de 1 segundo
+        ease: "power3.out",  // Suavização
+        //delay:.5,
+        scrollTrigger:{
+          trigger:'.paragrafo1-container',
+          start:'top center',
+        
+        }
+      });
+      gsap.to(".animated-paragrafo2", {
+        y: 0,  // Sobe para a posição original
+        duration: 1,  // Duração de 1 segundo
+        ease: "power3.out",  // Suavização
+        //delay:.5,
+        scrollTrigger:{
+          trigger:'.paragrafo2-container',
+          start:'top center',
+        
+        }
+      });
+      gsap.to(".animated-paragrafo3", {
+        y: 0,  // Sobe para a posição original
+        duration: 1,  // Duração de 1 segundo
+        ease: "power3.out",  // Suavização
+        //delay:.5,
+        scrollTrigger:{
+          trigger:'.paragrafo3-container',
+          start:'top center',
+        
+        }
+      });
+      gsap.to(".animated-paragrafo4", {
+        y: 0,  // Sobe para a posição original
+        duration: 1,  // Duração de 1 segundo
+        ease: "power3.out",  // Suavização
+        //delay:.5,
+        scrollTrigger:{
+          trigger:'.paragrafo4-container',
+          start:'top center',
+        
+        }
+      });
+      gsap.to(".animated-paragrafo5", {
+        y: 0,  // Sobe para a posição original
+        duration: 1,  // Duração de 1 segundo
+        ease: "power3.out",  // Suavização
+        //delay:.5,
+        scrollTrigger:{
+          trigger:'.paragrafo5-container',
+          start:'top center',
+        
+        }
+      });
+
+
+const icons = document.querySelectorAll('.lottie-icon');
+
+icons.forEach(iconElement => {
+  const iconName = iconElement.getAttribute('data-icon'); // Pega o nome do ícone
+  const animation = lottie.loadAnimation({
+    container: iconElement,
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: `/assets/media/icons/${iconName}.json` // Caminho dinâmico do ícone
+  });
+
+  // Animação no hover
+  iconElement.addEventListener('mouseenter', () => animation.play());
+  iconElement.addEventListener('mouseleave', () => animation.stop());
+});
+
